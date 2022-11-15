d04e0567da8d45113c4e7431915ff7edcd9a8878
Felipe <marconizero@gmail.com>
2022-11-15 09:32:28 -0300

    test: ensure that SignUpController returns 400 status if username is not provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,27 @@
+import { SignUpController } from './singup'
+import { MissingParamError } from './errors/missing-param-error'
+import { badRequest } from './helpers/http-helper'
+
+type SubTypes = {
+  sut: SignUpController
+}
+
+const makeSut = (): SubTypes => {
+  const sut = new SignUpController()
+  return {
+    sut
+  }
+}
+
+describe('Singup Controller', () => {
+  test('Should return 400 status if username is not provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('username')))
+  })
+})
