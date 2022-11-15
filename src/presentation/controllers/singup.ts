5a76b97a17f48828cc675167466f9b97d6bb6eee
Felipe <marconizero@gmail.com>
2022-11-15 18:03:53 -0300

    feat: ensure that SignUpController returns 500 status if UserValidator returns an error

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -2,22 +2,26 @@ import { Controller } from '../protocols/controller'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { Validator } from '../protocols/validator'
 import { MissingParamError } from './errors/missing-param-error'
-import { badRequest } from './helpers/http-helper'
+import { badRequest, serverError } from './helpers/http-helper'
 
 export class SignUpController implements Controller {
   constructor (private readonly userValidator: Validator) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['username', 'password']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const requiredFields = ['username', 'password']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
-    this.userValidator.isValid(httpRequest.body.username)
-    return {
-      status: 200,
-      body: ''
+      this.userValidator.isValid(httpRequest.body.username)
+      return {
+        status: 200,
+        body: ''
+      }
+    } catch (error: any) {
+      return serverError(error)
     }
   }
 }
