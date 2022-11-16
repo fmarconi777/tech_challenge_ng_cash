73329804e0a56cd8c8f9178833dac4237fe32bc4
Felipe Pedrosa <marconizero@gmail.com>
2022-11-16 15:35:55 -0300

    feat: ensure that UserValidatorAdapter returns true if validator returns true

--- src/util/validators/user-validator-adapter.ts
+++ src/util/validators/user-validator-adapter.ts
@@ -1,7 +1,11 @@
 import { Validator } from '../../presentation/protocols'
+import validator from 'validator'
 
 export class UserValidatorAdapter implements Validator {
   isValid (param: string): boolean {
+    if (validator.isAlphanumeric(param, 'pt-BR') && param.length >= 3) {
+      return true
+    }
     return false
   }
 }
