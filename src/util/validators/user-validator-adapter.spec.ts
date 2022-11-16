0c4b12b0e2d6ca9616ef50e67bb0d56e0a5429c1
Felipe Pedrosa <marconizero@gmail.com>
2022-11-16 15:36:16 -0300

    test: ensure that UserValidatorAdapter returns true if validator returns true

--- src/util/validators/user-validator-adapter.spec.ts
+++ src/util/validators/user-validator-adapter.spec.ts
@@ -1,9 +1,23 @@
 import { UserValidatorAdapter } from './user-validator-adapter'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isAlphanumeric (): boolean {
+    return true
+  }
+}))
 
 describe('UserValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
     const sut = new UserValidatorAdapter()
+    jest.spyOn(validator, 'isAlphanumeric').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_username')
     expect(isValid).toBeFalsy()
+  })
+
+  test('Should return true if validator returns true', () => {
+    const sut = new UserValidatorAdapter()
+    const isValid = sut.isValid('valid_username')
+    expect(isValid).toBeTruthy()
   })
 })
