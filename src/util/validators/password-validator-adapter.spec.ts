2d923b0724371aa471d48d676bee8442bbb31350
Felipe Pedrosa <marconizero@gmail.com>
2022-11-16 16:23:45 -0300

    test: ensure that PasswordValidatorAdapter calls validator with correct password

--- src/util/validators/password-validator-adapter.spec.ts
+++ src/util/validators/password-validator-adapter.spec.ts
@@ -11,13 +11,20 @@ describe('PasswordValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
     const sut = new PasswordValidatorAdapter()
     jest.spyOn(validator, 'isStrongPassword').mockReturnValueOnce(0)
-    const isValid = sut.isValid('any_password')
+    const isValid = sut.isValid('invalid_password')
     expect(isValid).toBeFalsy()
   })
 
   test('Should return true if validator returns true', () => {
     const sut = new PasswordValidatorAdapter()
-    const isValid = sut.isValid('any_password')
+    const isValid = sut.isValid('valid_password')
     expect(isValid).toBeTruthy()
+  })
+
+  test('Should call validator with correct password', () => {
+    const sut = new PasswordValidatorAdapter()
+    const isStrongPasswordSpy = jest.spyOn(validator, 'isStrongPassword')
+    sut.isValid('any_password')
+    expect(isStrongPasswordSpy).toHaveBeenCalledWith('any_password', { minSymbols: 0 })
   })
 })
