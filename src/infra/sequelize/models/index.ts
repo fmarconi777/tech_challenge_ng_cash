d21608d5a2edbc3795f70e40f22ff9682cb8426a
Felipe Pedrosa <marconizero@gmail.com>
2022-11-18 18:01:01 -0300

    fix: remove require and do import

--- src/infra/sequelize/models/index.ts
+++ src/infra/sequelize/models/index.ts
@@ -1,8 +1,9 @@
 import { Sequelize } from 'sequelize-typescript'
+import configData from '../config/config'
 
 const env = process.env.NODE_ENV ?? 'development'
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const config = require(__dirname + '../config/config.js')[env]
+const data = configData as any
+const config = data[env]
 
 const sequelize = config.uri ? new Sequelize(config.uri, config) : new Sequelize(config.database, config.username, config.password, config)
 
