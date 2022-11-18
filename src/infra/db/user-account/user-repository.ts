543bd2e0eca0fd31474e304e56218d97516c5365
Felipe Pedrosa <marconizero@gmail.com>
2022-11-18 14:29:32 -0300

    feat: ensure that UserRepository calls ORM with correct value

---
+++ src/infra/db/user-account/user-repository.ts
@@ -0,0 +1,14 @@
+import { CheckUserByUsernameRepository } from '../../../data/use-cases/add-user-account/db-add-user-account-protocols'
+import { UserModel } from '../../../domain/models/user'
+import { CheckUserByUserNameORM } from '../../protocols/check-user-by-user-name-orm'
+
+export class UserRepository implements CheckUserByUsernameRepository {
+  constructor (
+    private readonly checkUserByUserNameORM: CheckUserByUserNameORM
+  ) {}
+
+  async checkByUsername (username: string): Promise<UserModel | null> {
+    await this.checkUserByUserNameORM.checkByUsername(username)
+    return null
+  }
+}
---
+++ src/infra/protocols/check-user-by-user-name-orm.ts
@@ -0,0 +1,5 @@
+import { UserModel } from '../../domain/models/user'
+
+export interface CheckUserByUserNameORM {
+  checkByUsername: (username: string) => Promise<UserModel>
+}
