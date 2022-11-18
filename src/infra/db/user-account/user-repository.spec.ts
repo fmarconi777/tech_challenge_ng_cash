278b4af0460ea226f525d5dd80bd9f0dd5537ad7
Felipe Pedrosa <marconizero@gmail.com>
2022-11-18 14:30:03 -0300

    test: ensure that UserRepository calls ORM with correct value

---
+++ src/infra/db/user-account/user-repository.spec.ts
@@ -0,0 +1,42 @@
+import { UserModel } from '../../../domain/models/user'
+import { CheckUserByUserNameORM } from '../../protocols/check-user-by-user-name-orm'
+import { UserRepository } from './user-repository'
+
+const fakeUser = {
+  id: 'any_id',
+  username: 'any_user',
+  password: 'any_password',
+  accountId: 'any_account'
+}
+
+const makeCheckUserByUserNameORMStub = (): CheckUserByUserNameORM => {
+  class CheckUserByUserNameORMStub implements CheckUserByUserNameORM {
+    async checkByUsername (username: string): Promise<UserModel> {
+      return await Promise.resolve(fakeUser)
+    }
+  }
+  return new CheckUserByUserNameORMStub()
+}
+
+type SubTypes = {
+  sut: UserRepository
+  checkUserByUserNameORMStub: CheckUserByUserNameORM
+}
+
+const makeSut = (): SubTypes => {
+  const checkUserByUserNameORMStub = makeCheckUserByUserNameORMStub()
+  const sut = new UserRepository(checkUserByUserNameORMStub)
+  return {
+    sut,
+    checkUserByUserNameORMStub
+  }
+}
+
+describe('User Repository', () => {
+  test('Should call ORM with correct value', async () => {
+    const { sut, checkUserByUserNameORMStub } = makeSut()
+    const checkByUsernameSpy = jest.spyOn(checkUserByUserNameORMStub, 'checkByUsername')
+    await sut.checkByUsername('username')
+    expect(checkByUsernameSpy).toHaveBeenCalledWith('username')
+  })
+})
