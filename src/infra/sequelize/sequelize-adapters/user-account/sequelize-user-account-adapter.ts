24c46dba8f1f5fe49983232bcc04c989b266383e
Felipe <marconizero@gmail.com>
2022-11-19 01:49:47 -0300

    feat: ensure that SequelizeUserAccountAdapter returns a message on success

---
+++ src/infra/sequelize/sequelize-adapters/user-account/sequelize-user-account-adapter.ts
@@ -0,0 +1,32 @@
+import { UserData } from '../../../../domain/use-cases/add-user-account'
+import { AddUserAccountORM } from '../../../protocols/user-account/add-user-account-orm'
+import { ConnectionHelper } from '../../../db/helpers/connection-helper'
+import { Accounts } from '../../models/accounts'
+import { Users } from '../../models/users'
+
+export class SequelizeUserAccountAdapter implements AddUserAccountORM {
+  async addUserAccount (userData: UserData): Promise<string> {
+    let transaction
+    try {
+      transaction = await ConnectionHelper.client.transaction()
+
+      const balance: any = 100.00
+      const account = await Accounts.create({ balance }, { transaction })
+
+      await Users.create({
+        username: userData.username,
+        password: userData.password,
+        accountId: account.id
+      }, { transaction })
+
+      await transaction.commit()
+
+      return 'Account succesfully created'
+    } catch (error) {
+      if (transaction) {
+        await transaction.rollback()
+      }
+      throw error
+    }
+  }
+}
