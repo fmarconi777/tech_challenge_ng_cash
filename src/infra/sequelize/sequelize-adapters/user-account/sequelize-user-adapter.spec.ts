6c5c5d2a13ccb3c3f90738b322501eed00a67e10
Felipe <marconizero@gmail.com>
2022-11-19 02:09:04 -0300

    test: ensure that SequelizeUserAdapter returns an user on success

--- src/infra/sequelize/sequelize-adapters/user-account/sequelize-user-adapter.spec.ts
+++ src/infra/sequelize/sequelize-adapters/user-account/sequelize-user-adapter.spec.ts
@@ -1,5 +1,6 @@
 import { SequelizeUserAdapter } from './sequelize-user-adapter'
 import { ConnectionHelper } from '../../../db/helpers/connection-helper'
+import { SequelizeUserAccountAdapter } from './sequelize-user-account-adapter'
 
 describe('SequelizeUser Adapter', () => {
   beforeAll(async () => {
@@ -14,5 +15,17 @@ describe('SequelizeUser Adapter', () => {
     const sut = new SequelizeUserAdapter()
     const user = await sut.checkByUsername('invalid_username')
     expect(user).toBeNull()
+  })
+
+  test('Should return an user on success', async () => {
+    const sut = new SequelizeUserAdapter()
+    const sequelizeUserAccount = new SequelizeUserAccountAdapter()
+    await sequelizeUserAccount.addUserAccount({ username: 'valid_username', password: 'hashed_password' })
+    const user = await sut.checkByUsername('valid_username')
+    expect(user).toBeTruthy()
+    expect(user?.id).toBeTruthy()
+    expect(user?.accountId).toBeTruthy()
+    expect(user?.username).toBe('valid_username')
+    expect(user?.password).toBe('hashed_password')
   })
 })
