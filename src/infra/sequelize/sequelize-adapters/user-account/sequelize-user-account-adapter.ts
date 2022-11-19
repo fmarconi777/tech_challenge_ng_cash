264c63d01cee5b264fe016b0cfff4c3f03c7b308
Felipe <marconizero@gmail.com>
2022-11-19 19:07:46 -0300

    refactor: move sequelize-adapters protocols to a single file

--- src/infra/sequelize/sequelize-adapters/user-account/sequelize-user-account-adapter.ts
+++ src/infra/sequelize/sequelize-adapters/user-account/sequelize-user-account-adapter.ts
@@ -1,5 +1,5 @@
 import { UserData } from '../../../../domain/use-cases/add-user-account'
-import { AddUserAccountORM } from '../../../protocols/user-account/add-user-account-orm'
+import { AddUserAccountORM } from '../sequelize-adapters-protocols'
 import { ConnectionHelper } from '../../../db/helpers/connection-helper'
 import { Accounts } from '../../models/accounts'
 import { Users } from '../../models/users'
--- src/infra/sequelize/sequelize-adapters/user-account/sequelize-user-adapter.ts
+++ src/infra/sequelize/sequelize-adapters/user-account/sequelize-user-adapter.ts
@@ -1,10 +1,10 @@
-import { CheckUserByUsernameRepository } from '../../../../data/protocols/db/user-account/check-user-by-username-repository'
+import { CheckUserByUserNameORM } from '../sequelize-adapters-protocols'
 import { UserModel } from '../../../../domain/models/user'
 import { Users } from '../../models/users'
-import { parseUser } from '../../../../util/parsers/parse-user'
+import { parseUser } from '../sequelize-parsers/parse-user'
 import { ConnectionHelper } from '../../../db/helpers/connection-helper'
 
-export class SequelizeUserAdapter implements CheckUserByUsernameRepository {
+export class SequelizeUserAdapter implements CheckUserByUserNameORM {
   async checkByUsername (username: string): Promise<UserModel | null> {
     await ConnectionHelper.reconnect()
     const user = await Users.findOne({ where: { username }, raw: true })
