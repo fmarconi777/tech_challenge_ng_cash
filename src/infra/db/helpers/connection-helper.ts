17196f2507212b9c0c6fc4fc240895c6ad1d06d3
Felipe <marconizero@gmail.com>
2022-11-19 00:56:11 -0300

    feat: create methods to connect and disconnect to database

---
+++ src/infra/db/helpers/connection-helper.ts
@@ -0,0 +1,30 @@
+import { Sequelize } from 'sequelize'
+import connection from '../../sequelize/models/index'
+
+const sequelizeClient = connection.sequelize
+
+export const ConnectionHelper = {
+  client: null as unknown as Sequelize,
+
+  environment: '',
+
+  async connect (env: string): Promise<void> {
+    this.environment = env
+    process.env.NODE_ENV = env
+    this.client = sequelizeClient
+    await this.client.authenticate()
+  },
+
+  async disconnect (): Promise<void> {
+    if (this.client !== null) {
+      await this.client.close()
+    }
+    this.client = null as unknown as Sequelize
+  },
+
+  async reconnect (): Promise<void> {
+    if (!this.client?.authenticate()) { // eslint-disable-line
+      await this.connect(this.environment)
+    }
+  }
+}
