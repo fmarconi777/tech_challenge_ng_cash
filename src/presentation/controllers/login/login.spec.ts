e09458dc928130dc0986d6b71b38c628a0078c5d
Felipe <marconizero@gmail.com>
2022-11-19 23:14:06 -0300

    test: ensure that LoginController returns 400 status if no username is provided

---
+++ src/presentation/controllers/login/login.spec.ts
@@ -0,0 +1,27 @@
+import { MissingParamError } from '../errors'
+import { badRequest } from '../helpers/http-helper'
+import { LoginController } from './login'
+
+type SubTypes = {
+  sut: LoginController
+}
+
+const makeSut = (): SubTypes => {
+  const sut = new LoginController()
+  return {
+    sut
+  }
+}
+
+describe('Login Controller', () => {
+  test('Should return 400 status if no username is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const response = await sut.handle(httpRequest)
+    expect(response).toEqual(badRequest(new MissingParamError('username')))
+  })
+})
