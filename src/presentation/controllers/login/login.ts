fa191e16fe0cee38ecf2928c0e16152bf1f23f17
Felipe <marconizero@gmail.com>
2022-11-19 23:47:21 -0300

    feat: ensure that LoginController returns 500 status if Authentication returns an error

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,23 +1,27 @@
 import { Authentication } from '../../../domain/use-cases/authentication'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { MissingParamError } from '../errors'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, serverError } from '../helpers/http-helper'
 
 export class LoginController implements Controller {
   constructor (private readonly authentication: Authentication) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['username', 'password']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const requiredFields = ['username', 'password']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
-    const { username, password } = httpRequest.body
-    await this.authentication.auth(username, password)
-    return {
-      status: 200,
-      body: ''
+      const { username, password } = httpRequest.body
+      await this.authentication.auth(username, password)
+      return {
+        status: 200,
+        body: ''
+      }
+    } catch (error: any) {
+      return serverError()
     }
   }
 }
