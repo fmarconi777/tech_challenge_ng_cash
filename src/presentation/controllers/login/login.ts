0c6c7f6ea84bf8bc949207a5cd3afbebf969d82b
Felipe <marconizero@gmail.com>
2022-11-19 23:13:42 -0300

    feat: ensure that LoginController returns 400 status if no username is provided

---
+++ src/presentation/controllers/login/login.ts
@@ -0,0 +1,18 @@
+import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { MissingParamError } from '../errors'
+import { badRequest } from '../helpers/http-helper'
+
+export class LoginController implements Controller {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const requiredFields = ['username']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
+    }
+    return {
+      status: 200,
+      body: ''
+    }
+  }
+}
