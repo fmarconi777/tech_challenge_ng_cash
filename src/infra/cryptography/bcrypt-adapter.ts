37bc322396b0b97aa7e6d2aa5e4d642aecffb559
Felipe <marconizero@gmail.com>
2022-11-20 09:48:39 -0300

    feat: ensure that BcryptAdapter calls compare with correct values

--- src/infra/cryptography/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -1,10 +1,16 @@
 import { Hasher } from '../../data/protocols/crytography/hasher'
 import bcrypt from 'bcrypt'
 import 'dotenv/config'
+import { HashComparer } from '../../data/protocols/crytography/hash-comparer'
 
-export class BcryptAdapter implements Hasher {
+export class BcryptAdapter implements Hasher, HashComparer {
   private readonly salt: number = +(process.env.SALT as string)
   async hash (value: string): Promise<string> {
     return await bcrypt.hash(value, this.salt)
+  }
+
+  async compare (password: string, hash: string): Promise<boolean> {
+    await bcrypt.compare(password, hash)
+    return true
   }
 }
