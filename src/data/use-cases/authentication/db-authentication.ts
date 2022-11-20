728c82c65ca621b2dcf0e5ef8cf7e55e9e8ba7a4
Felipe <marconizero@gmail.com>
2022-11-20 01:43:28 -0300

    feat: ensure that DbAuthentication calls HashComparer with correct password

--- src/data/use-cases/authentication/db-authentication.ts
+++ src/data/use-cases/authentication/db-authentication.ts
@@ -1,14 +1,17 @@
 import { Authentication, AuthenticationModel } from '../../../domain/use-cases/authentication'
+import { HashComparer } from '../../protocols/crytography/hash-comparer'
 import { LoadUserByUsernameRepository } from '../../protocols/db/user/load-user-by-username-repository'
 
 export class DbAuthentication implements Authentication {
   constructor (
-    private readonly loadUserByUsernameRepository: LoadUserByUsernameRepository
+    private readonly loadUserByUsernameRepository: LoadUserByUsernameRepository,
+    private readonly hashComparer: HashComparer
   ) {}
 
   async auth (authenticationParams: AuthenticationModel): Promise<string | null> {
     const user = await this.loadUserByUsernameRepository.load(authenticationParams.username)
     if (user) {
+      await this.hashComparer.compare(authenticationParams.password, user.password)
       return ''
     }
     return null
