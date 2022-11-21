b987b93b31c0c253573d15c8fdedd85599c8b7d0
Felipe Pedrosa <marconizero@gmail.com>
2022-11-21 17:59:52 -0300

    feat: ensure that SequelizeUserAdapter returns null if user does not exist

--- src/infra/sequelize/sequelize-adapters/user/sequelize-user-adapter.ts
+++ src/infra/sequelize/sequelize-adapters/user/sequelize-user-adapter.ts
@@ -3,11 +3,16 @@ import { UserModel } from '../../../../domain/models/user'
 import { Users } from '../../models/users'
 import { parseUser } from '../sequelize-parsers/parse-user'
 import { ConnectionHelper } from '../../../db/helpers/connection-helper'
+import { CheckUserByIdORM } from '../../../protocols/user/check-user-by-id-orm'
 
-export class SequelizeUserAdapter implements CheckUserByUserNameORM {
+export class SequelizeUserAdapter implements CheckUserByUserNameORM, CheckUserByIdORM {
   async checkByUsername (username: string): Promise<UserModel | null> {
     await ConnectionHelper.reconnect()
     const user = await Users.findOne({ where: { username }, raw: true })
     return parseUser(user)
+  }
+
+  async checkById (id: number): Promise<UserModel | null> {
+    return null
   }
 }
