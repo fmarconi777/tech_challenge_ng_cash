666daff62b5d0081eb931aa9aed56fb0ce439770
Felipe Pedrosa <marconizero@gmail.com>
2022-11-21 15:01:56 -0300

    feat: ensure that BalanceController throws if LoadBalance throws

--- src/presentation/controllers/balance/balance.ts
+++ src/presentation/controllers/balance/balance.ts
@@ -1,14 +1,19 @@
 import { LoadBalance } from '../../../domain/use-cases/load-balance/load-balance'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { serverError } from '../helpers/http-helper'
 
 export class BalanceController implements Controller {
   constructor (private readonly loadBalance: LoadBalance) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadBalance.load(+httpRequest.userId)
-    return {
-      status: 200,
-      body: ''
+    try {
+      await this.loadBalance.load(+httpRequest.userId)
+      return {
+        status: 200,
+        body: ''
+      }
+    } catch (error: any) {
+      return serverError()
     }
   }
 }
