8e951ea466dd73f683a93c70aa2a26292172ee77
Felipe Pedrosa <marconizero@gmail.com>
2022-11-21 15:58:43 -0300

    test: ensure that DbLoadBalance calls CheckUserByIdRepository with correct value

---
+++ src/data/use-cases/load-balance/db-load-balance.spec.ts
@@ -0,0 +1,37 @@
+import { UserModel } from '../../../domain/models/user'
+import { CheckUserByIdRepository } from '../../protocols/db/user/check-user-by-id-repository'
+import { DbLoadBalance } from './db-load-balance'
+
+const userId = 1
+
+const makeCheckUserByIdRepositoryStub = (): CheckUserByIdRepository => {
+  class CheckUserByIdRepositorySutb implements CheckUserByIdRepository {
+    async checkById (id: number): Promise<UserModel | null> {
+      return await Promise.resolve(null)
+    }
+  }
+  return new CheckUserByIdRepositorySutb()
+}
+
+type SubTypes = {
+  sut: DbLoadBalance
+  checkUserByIdRepositoryStub: CheckUserByIdRepository
+}
+
+const makeSut = (): SubTypes => {
+  const checkUserByIdRepositoryStub = makeCheckUserByIdRepositoryStub()
+  const sut = new DbLoadBalance(checkUserByIdRepositoryStub)
+  return {
+    sut,
+    checkUserByIdRepositoryStub
+  }
+}
+
+describe('DbLoadBalance', () => {
+  test('Should call CheckUserByIdRepository with correct value', async () => {
+    const { sut, checkUserByIdRepositoryStub } = makeSut()
+    const checkByUsernameSpy = jest.spyOn(checkUserByIdRepositoryStub, 'checkById')
+    await sut.load(userId)
+    expect(checkByUsernameSpy).toHaveBeenCalledWith(userId)
+  })
+})
