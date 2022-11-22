d9c07ebe7bf0dc0deab925742a862677b112dc9a
Felipe Pedrosa <marconizero@gmail.com>
2022-11-22 14:04:50 -0300

    feat: ensure that DbLoadUserByToken calls LoadUserByIdRepository with correct value

--- src/data/use-cases/load-user-by-token/db-load-user-by-token.ts
+++ src/data/use-cases/load-user-by-token/db-load-user-by-token.ts
@@ -1,13 +1,20 @@
 import { UserModel } from '../../../domain/models/user'
 import { LoadUserByToken } from '../../../domain/use-cases/load-user-by-token/load-user-by-token'
 import { Decrypter } from '../../protocols/crytography/decrypter'
+import { LoadUserByIdRepository } from '../../protocols/db/user/load-user-by-id-repository'
 
 export class DbLoadUserByToken implements LoadUserByToken {
-  constructor (private readonly decrypter: Decrypter) {
+  constructor (
+    private readonly decrypter: Decrypter,
+    private readonly loadUserByIdRepository: LoadUserByIdRepository
+  ) {
   }
 
-  async load (accessToken: string, role?: string | undefined): Promise<UserModel | null> {
-    await this.decrypter.decrypt(accessToken)
+  async load (accessToken: string): Promise<UserModel | null> {
+    const payload: any = await this.decrypter.decrypt(accessToken)
+    if (payload) {
+      await this.loadUserByIdRepository.loadById(payload.id)
+    }
     return null
   }
 }
