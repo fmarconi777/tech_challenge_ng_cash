8df00fa00639dd664bf0f5fcb4b94154140c00d8
Felipe Pedrosa <marconizero@gmail.com>
2022-11-22 08:02:33 -0300

    test: ensure that SequelizeAccountAdapter returns null an account on success

--- src/infra/sequelize/sequelize-adapters/account/sequelize-account-adapter.spec.ts
+++ src/infra/sequelize/sequelize-adapters/account/sequelize-account-adapter.spec.ts
@@ -1,6 +1,7 @@
 import { ConnectionHelper } from '../../../db/helpers/connection-helper'
 import { Accounts } from '../../models/accounts'
 import { Users } from '../../models/users'
+import { SequelizeUserAccountAdapter } from '../user-account/sequelize-user-account-adapter'
 import { SequelizeAccountAdapter } from './sequelize-account-adapter'
 
 describe('SequelizeAccount Adapter', () => {
@@ -27,7 +28,18 @@ describe('SequelizeAccount Adapter', () => {
 
   test('Should return null if account does not exist', async () => {
     const sut = new SequelizeAccountAdapter()
-    const user = await sut.loadById(158922357855)
-    expect(user).toBeNull()
+    const account = await sut.loadById(158922357855)
+    expect(account).toBeNull()
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new SequelizeAccountAdapter()
+    const sequelizeUserAccount = new SequelizeUserAccountAdapter()
+    await sequelizeUserAccount.addUserAccount({ username: 'valid_username', password: 'hashed_password' })
+    const lastAccount = ((await Accounts.findAll())[0])
+    const account = await sut.loadById(lastAccount.id)
+    expect(account).toBeTruthy()
+    expect(account?.id).toBeTruthy()
+    expect(account?.balance).toEqual('100.00')
   })
 })
