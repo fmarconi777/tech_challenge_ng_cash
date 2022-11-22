250058fc0ca4015490933559f69972f5f1816c73
Felipe Pedrosa <marconizero@gmail.com>
2022-11-22 14:52:50 -0300

    feat: ensure that JwtAdapter calls verify with correct value

--- src/infra/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -1,11 +1,17 @@
 import { Encrypter } from '../../../data/protocols/crytography/encrypter'
 import jwt from 'jsonwebtoken'
 import 'dotenv/config'
+import { Decrypter } from '../../../data/protocols/crytography/decrypter'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   private readonly secreteKey = process.env.SECRET_KEY
 
   async encrypt (id: string): Promise<string> {
     return jwt.sign({ id }, (this.secreteKey as string), { expiresIn: '1d' })
+  }
+
+  async decrypt (value: string): Promise<any> {
+    jwt.verify(value, (this.secreteKey as string))
+    return null
   }
 }
