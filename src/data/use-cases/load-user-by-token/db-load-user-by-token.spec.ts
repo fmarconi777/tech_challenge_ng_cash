a13cfaa19986372540a4323c5b55a8d469fcec78
Felipe Pedrosa <marconizero@gmail.com>
2022-11-22 14:05:26 -0300

    test: ensure that DbLoadUserByToken calls LoadUserByIdRepository with correct value

--- src/data/use-cases/load-user-by-token/db-load-user-by-token.spec.ts
+++ src/data/use-cases/load-user-by-token/db-load-user-by-token.spec.ts
@@ -1,26 +1,47 @@
+import { UserModel } from '../../../domain/models/user'
 import { Decrypter } from '../../protocols/crytography/decrypter'
+import { LoadUserByIdRepository } from '../../protocols/db/user/load-user-by-id-repository'
 import { DbLoadUserByToken } from './db-load-user-by-token'
+
+const fakeUser = {
+  id: '1',
+  username: 'any_username',
+  password: 'any_password',
+  accountId: '1'
+}
 
 const makeDecrypterStub = (): Decrypter => {
   class DecrypterStub implements Decrypter {
-    async decrypt (token: string): Promise<string> {
-      return await Promise.resolve('any_value')
+    async decrypt (token: string): Promise<any | null> {
+      return await Promise.resolve({ id: 'any_id' })
     }
   }
   return new DecrypterStub()
 }
 
+const makeLoadUserByIdRepositoryStub = (): LoadUserByIdRepository => {
+  class LoadUserByIdRepositoryStub implements LoadUserByIdRepository {
+    async loadById (id: number): Promise<UserModel | null> {
+      return await Promise.resolve(fakeUser)
+    }
+  }
+  return new LoadUserByIdRepositoryStub()
+}
+
 type SubTypes = {
   sut: DbLoadUserByToken
   decrypterStub: Decrypter
+  loadUserByIdRepositoryStub: LoadUserByIdRepository
 }
 
 const makeSut = (): SubTypes => {
+  const loadUserByIdRepositoryStub = makeLoadUserByIdRepositoryStub()
   const decrypterStub = makeDecrypterStub()
-  const sut = new DbLoadUserByToken(decrypterStub)
+  const sut = new DbLoadUserByToken(decrypterStub, loadUserByIdRepositoryStub)
   return {
     sut,
-    decrypterStub
+    decrypterStub,
+    loadUserByIdRepositoryStub
   }
 }
 
@@ -28,21 +49,28 @@ describe('DbLoadUserByToken', () => {
   test('Should call Decrypter with correct value', async () => {
     const { sut, decrypterStub } = makeSut()
     const decryptSpy = jest.spyOn(decrypterStub, 'decrypt')
-    await sut.load('any_token', 'any_role')
+    await sut.load('any_token')
     expect(decryptSpy).toHaveBeenCalledWith('any_token')
   })
 
   test('Should throw if Decrypter throws', async () => {
     const { sut, decrypterStub } = makeSut()
     jest.spyOn(decrypterStub, 'decrypt').mockReturnValueOnce(Promise.reject(new Error()))
-    const user = sut.load('any_token', 'any_role')
+    const user = sut.load('any_token')
     await expect(user).rejects.toThrow()
   })
 
   test('Should return null if Decrypter returns null', async () => {
     const { sut, decrypterStub } = makeSut()
     jest.spyOn(decrypterStub, 'decrypt').mockReturnValueOnce(Promise.resolve(''))
-    const user = await sut.load('any_token', 'any_role')
+    const user = await sut.load('any_token')
     expect(user).toBeNull()
+  })
+
+  test('Should call LoadUserByIdRepository with correct value', async () => {
+    const { sut, loadUserByIdRepositoryStub } = makeSut()
+    const decryptSpy = jest.spyOn(loadUserByIdRepositoryStub, 'loadById')
+    await sut.load('any_token')
+    expect(decryptSpy).toHaveBeenCalledWith('any_id')
   })
 })
