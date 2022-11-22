9e7ae859802b1150f5647a40071840ed4f712399
Felipe Pedrosa <marconizero@gmail.com>
2022-11-22 08:01:48 -0300

    feat: ensure that SequelizeAccountAdapter returns null an account on success

--- src/infra/sequelize/sequelize-adapters/account/sequelize-account-adapter.ts
+++ src/infra/sequelize/sequelize-adapters/account/sequelize-account-adapter.ts
@@ -1,8 +1,19 @@
 import { AccountModel } from '../../../../domain/models/account'
 import { LoadAccountByIdORM } from '../../../protocols/account/load-account-by-id-orm'
+import { Accounts } from '../../models/accounts'
 
 export class SequelizeAccountAdapter implements LoadAccountByIdORM {
   async loadById (id: number): Promise<AccountModel | null> {
+    return this.parseAccount(await Accounts.findByPk(id, { raw: true }))
+  }
+
+  parseAccount (account: any): AccountModel | null {
+    if (account) {
+      return {
+        id: String(account.id),
+        balance: String(account.balance)
+      }
+    }
     return null
   }
 }
