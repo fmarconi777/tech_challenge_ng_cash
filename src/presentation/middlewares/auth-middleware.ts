a47a902449f277fe40466418b760748e0086f405
Felipe Pedrosa <marconizero@gmail.com>
2022-11-22 10:21:45 -0300

    feat: ensure that AuthMiddleware returns 500 if LoadUserByToken throws

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,6 +1,6 @@
 import { LoadUserByToken } from '../../domain/use-cases/load-user-by-token/load-user-by-token'
 import { AccessDeniedError } from '../errors'
-import { forbidden, okResponse } from '../helpers/http-helper'
+import { forbidden, okResponse, serverError } from '../helpers/http-helper'
 import { HttpRequest, HttpResponse, Middleware } from '../protocols'
 
 export class AuthMiddleware implements Middleware {
@@ -10,13 +10,17 @@ export class AuthMiddleware implements Middleware {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const accessToken = httpRequest.header
-    if (accessToken && accessToken !== 'undefined') {
-      const user = await this.loadUserByToken.load(httpRequest.header, this.role)
-      if (user) {
-        return okResponse('')
+    try {
+      const accessToken = httpRequest.header
+      if (accessToken && accessToken !== 'undefined') {
+        const user = await this.loadUserByToken.load(httpRequest.header, this.role)
+        if (user) {
+          return okResponse('')
+        }
       }
+      return forbidden(new AccessDeniedError())
+    } catch (error: any) {
+      return serverError()
     }
-    return forbidden(new AccessDeniedError())
   }
 }
