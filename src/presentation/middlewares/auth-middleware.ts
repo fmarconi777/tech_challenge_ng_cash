68d0f773ca3e706a39e4e8a1b4aa24de709ff271
Felipe Pedrosa <marconizero@gmail.com>
2022-11-22 10:16:18 -0300

    feat: ensure that AuthMiddleware returns 403 if LoadUserByToken returns null

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,18 +1,21 @@
-import { LoadAccountByToken } from '../../domain/use-cases/load-account-by-token/load-account-by-token'
+import { LoadUserByToken } from '../../domain/use-cases/load-user-by-token/load-user-by-token'
 import { AccessDeniedError } from '../errors'
-import { forbidden } from '../helpers/http-helper'
+import { forbidden, okResponse } from '../helpers/http-helper'
 import { HttpRequest, HttpResponse, Middleware } from '../protocols'
 
 export class AuthMiddleware implements Middleware {
   constructor (
-    private readonly loadAccountByToken: LoadAccountByToken,
+    private readonly loadUserByToken: LoadUserByToken,
     private readonly role?: string
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const accessToken = httpRequest.header
     if (accessToken && accessToken !== 'undefined') {
-      await this.loadAccountByToken.load(httpRequest.header, this.role)
+      const user = await this.loadUserByToken.load(httpRequest.header, this.role)
+      if (user) {
+        return okResponse('')
+      }
     }
     return forbidden(new AccessDeniedError())
   }
