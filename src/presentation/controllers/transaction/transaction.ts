4f90c4b09e370c83f4fd6c0c2a29c5d0981a70d6
Felipe Pedrosa <marconizero@gmail.com>
2022-11-23 14:09:36 -0300

    feat: ensure that TransactionController returns 500 status if CurrencyValidator throws

--- src/presentation/controllers/transaction/transaction.ts
+++ src/presentation/controllers/transaction/transaction.ts
@@ -1,6 +1,6 @@
 import { RecordTransaction } from '../../../domain/use-cases/transaction/record-transaction'
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest, okResponse } from '../../helpers/http-helper'
+import { badRequest, okResponse, serverError } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse, Validator } from '../../protocols'
 
 export class TransactionController implements Controller {
@@ -10,24 +10,28 @@ export class TransactionController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['cashInUsername', 'credit']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const requiredFields = ['cashInUsername', 'credit']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
+      const { username } = httpRequest.user
+      const { cashInUsername, credit } = httpRequest.body
+      if (username === cashInUsername) {
+        return badRequest(new InvalidParamError('cashInUsername'))
+      }
+      this.currencyValidator.isValid(credit)
+      const transactionData = {
+        cashOutUsername: username,
+        cashInUsername,
+        credit
+      }
+      await this.recordTransaction.record(transactionData)
+      return okResponse('')
+    } catch (error: any) {
+      return serverError()
     }
-    const { username } = httpRequest.user
-    const { cashInUsername, credit } = httpRequest.body
-    if (username === cashInUsername) {
-      return badRequest(new InvalidParamError('cashInUsername'))
-    }
-    this.currencyValidator.isValid(credit)
-    const transactionData = {
-      cashOutUsername: username,
-      cashInUsername,
-      credit
-    }
-    await this.recordTransaction.record(transactionData)
-    return okResponse('')
   }
 }
