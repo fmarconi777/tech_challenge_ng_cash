e8c664fdbfd3f715d20cb35d0e1e113ce4b36fc0
Felipe Pedrosa <marconizero@gmail.com>
2022-11-23 16:50:38 -0300

    feat: ensure DbRecordTransaction calls LoadAccountByIdRepository with correct value

--- src/data/use-cases/transaction/db-record-transaction.ts
+++ src/data/use-cases/transaction/db-record-transaction.ts
@@ -1,14 +1,18 @@
 import { Record, RecordTransaction, TransactionData } from '../../../domain/use-cases/transaction/record-transaction'
+import { LoadAccountByIdRepository } from '../../protocols/db/account/load-account-by-id-repository'
 import { LoadUserByUsernameRepository } from '../../protocols/db/user/load-user-by-username-repository'
 
 export class DbRecordTransaction implements RecordTransaction {
   constructor (
-    private readonly loadUserByUsernameRepository: LoadUserByUsernameRepository
+    private readonly loadUserByUsernameRepository: LoadUserByUsernameRepository,
+    private readonly loadAccountByIdRepository: LoadAccountByIdRepository
   ) {}
 
   async record (transactionData: TransactionData): Promise<Record> {
-    const user = await this.loadUserByUsernameRepository.loadByUsername(transactionData.cashInUsername)
-    if (user) {
+    const cashInUser = await this.loadUserByUsernameRepository.loadByUsername(transactionData.cashInUsername)
+    if (cashInUser) {
+      const cashOutUser: any = await this.loadUserByUsernameRepository.loadByUsername(transactionData.cashOutUsername)
+      await this.loadAccountByIdRepository.loadById(+cashOutUser.accountId)
       return {
         recorded: true,
         message: ''
