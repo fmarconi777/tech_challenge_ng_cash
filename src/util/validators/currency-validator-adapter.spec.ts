b14941869a72033c9d17b27b5119e49b7e2c21fc
Felipe Pedrosa <marconizero@gmail.com>
2022-11-23 15:10:05 -0300

    test: ensure CurrencyValidatorAdapter returns false if validator returns false

---
+++ src/util/validators/currency-validator-adapter.spec.ts
@@ -0,0 +1,17 @@
+import { CurrencyValidatorAdapter } from './currency-validator-adapter'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isCurrency (): boolean {
+    return true
+  }
+}))
+
+describe('CurrencyValidator Adapter', () => {
+  test('Should return false if validator returns false', () => {
+    const sut = new CurrencyValidatorAdapter()
+    jest.spyOn(validator, 'isCurrency').mockReturnValueOnce(false)
+    const isValid = sut.isValid('1.000.00')
+    expect(isValid).toBeFalsy()
+  })
+})
