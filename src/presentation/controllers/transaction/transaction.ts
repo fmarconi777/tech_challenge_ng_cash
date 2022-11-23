a983581c617c0f6817430aa377485414cdac40f1
Felipe Pedrosa <marconizero@gmail.com>
2022-11-23 12:22:22 -0300

    feat: ensure that TransactionController returns 400 status if username and cashInUsername are equal

--- src/presentation/controllers/transaction/transaction.ts
+++ src/presentation/controllers/transaction/transaction.ts
@@ -1,12 +1,16 @@
-import { MissingParamError } from '../../errors'
+import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest, okResponse } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 
 export class TransactionController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { username } = httpRequest.body
-    if (!username) {
-      return badRequest(new MissingParamError('username'))
+    const { username } = httpRequest.user
+    const { cashInUsername } = httpRequest.body
+    if (!cashInUsername) {
+      return badRequest(new MissingParamError('cashInUsername'))
+    }
+    if (username === cashInUsername) {
+      return badRequest(new InvalidParamError('cashInUsername'))
     }
     return okResponse('')
   }
