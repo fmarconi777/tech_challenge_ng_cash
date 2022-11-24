17e2ea9f23989fb39800cd06c9385563352120c6
Felipe Pedrosa <marconizero@gmail.com>
2022-11-24 13:34:03 -0300

    feat: ensure LoginController returns 405 status if not allowed method is called

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,26 +1,32 @@
 import { Controller, HttpRequest, HttpResponse, Authentication } from './login-protocols'
 import { MissingParamError } from '../../errors'
-import { badRequest, okResponse, serverError, unauthorized } from '../../helpers/http-helper'
+import { badRequest, methodNotAllowed, okResponse, serverError, unauthorized } from '../../helpers/http-helper'
 
 export class LoginController implements Controller {
   constructor (private readonly authentication: Authentication) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    try {
-      const requiredFields = ['username', 'password']
-      for (const field of requiredFields) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field))
+    const method = httpRequest.method
+    switch (method) {
+      case 'POST':
+        try {
+          const requiredFields = ['username', 'password']
+          for (const field of requiredFields) {
+            if (!httpRequest.body[field]) {
+              return badRequest(new MissingParamError(field))
+            }
+          }
+          const { username, password } = httpRequest.body
+          const accessToken = await this.authentication.auth({ username, password })
+          if (!accessToken) {
+            return unauthorized()
+          }
+          return okResponse(accessToken)
+        } catch (error: any) {
+          return serverError()
         }
-      }
-      const { username, password } = httpRequest.body
-      const accessToken = await this.authentication.auth({ username, password })
-      if (!accessToken) {
-        return unauthorized()
-      }
-      return okResponse(accessToken)
-    } catch (error: any) {
-      return serverError()
+      default:
+        return methodNotAllowed()
     }
   }
 }
