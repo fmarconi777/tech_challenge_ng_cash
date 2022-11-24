5fd9727a91d12e56cb84c322d1a2c5a1d5a50adb
Felipe Pedrosa <marconizero@gmail.com>
2022-11-24 11:00:34 -0300

    test: ensure SequelizeUserAccountAdapter reconnects if connection is lost

--- src/infra/sequelize/sequelize-adapters/transaction/sequelize-transaction-adapter.spec.ts
+++ src/infra/sequelize/sequelize-adapters/transaction/sequelize-transaction-adapter.spec.ts
@@ -4,6 +4,7 @@ import { Transactions } from '../../models/transactions'
 import { Users } from '../../models/users'
 import { SequelizeTransactionAdapter } from './sequelize-transaction-adapter'
 import { SequelizeUserAccountAdapter } from '../user-account/sequelize-user-account-adapter'
+import { Sequelize } from 'sequelize'
 
 describe('Sequelize Tansaction Adapter', () => {
   beforeAll(async () => {
@@ -33,6 +34,30 @@ describe('Sequelize Tansaction Adapter', () => {
   })
 
   test('Should return a message on success', async () => {
+    const sut = new SequelizeTransactionAdapter()
+    const creatUserAccount = new SequelizeUserAccountAdapter()
+    await creatUserAccount.addUserAccount({
+      username: 'valid_username',
+      password: 'hashed_password'
+    })
+    await creatUserAccount.addUserAccount({
+      username: 'valid_username2',
+      password: 'hashed_password'
+    })
+    const users = await Users.findAll()
+    const recordData = {
+      debitedAccountId: users[0].accountId,
+      debitedBalance: 0.00,
+      creditedAccountId: users[1].accountId,
+      creditedBalance: 200.00,
+      value: 100.00
+    }
+    const record = await sut.record(recordData)
+    expect(record).toBe('Transaction succesfully recorded')
+  })
+
+  test('Should reconnect if connection is lost', async () => {
+    ConnectionHelper.client = null as unknown as Sequelize
     const sut = new SequelizeTransactionAdapter()
     const creatUserAccount = new SequelizeUserAccountAdapter()
     await creatUserAccount.addUserAccount({
