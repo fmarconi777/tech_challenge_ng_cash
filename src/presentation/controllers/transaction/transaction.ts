5f555d7c2a0375391c3f52aadbe1a4d533005fc1
Felipe Pedrosa <marconizero@gmail.com>
2022-11-24 13:51:03 -0300

    feat: ensure httpRequest returns 405 status if not allowed method is called

--- src/presentation/controllers/transaction/transaction.ts
+++ src/presentation/controllers/transaction/transaction.ts
@@ -1,7 +1,7 @@
 import { RecordTransaction } from '../../../domain/use-cases/transaction/record-transaction'
 import { InvalidParamError, MissingParamError } from '../../errors'
 import { TransactionError } from '../../errors/transaction-error'
-import { badRequest, okResponse, serverError } from '../../helpers/http-helper'
+import { badRequest, methodNotAllowed, okResponse, serverError } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse, Validator } from '../../protocols'
 
 export class TransactionController implements Controller {
@@ -11,33 +11,39 @@ export class TransactionController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    try {
-      const requiredFields = ['creditedUsername', 'value']
-      for (const field of requiredFields) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field))
+    const method = httpRequest.method
+    switch (method) {
+      case 'POST':
+        try {
+          const requiredFields = ['creditedUsername', 'value']
+          for (const field of requiredFields) {
+            if (!httpRequest.body[field]) {
+              return badRequest(new MissingParamError(field))
+            }
+          }
+          const { username } = httpRequest.user
+          const { creditedUsername, value } = httpRequest.body
+          if (username === creditedUsername) {
+            return badRequest(new InvalidParamError('creditedUsername'))
+          }
+          if (!this.currencyValidator.isValid(value)) {
+            return badRequest(new InvalidParamError('value'))
+          }
+          const transactionData = {
+            debitedUsername: username,
+            creditedUsername,
+            value
+          }
+          const record = await this.recordTransaction.record(transactionData)
+          if (!record.recorded) {
+            return badRequest(new TransactionError(record.message))
+          }
+          return okResponse(record.message)
+        } catch (error: any) {
+          return serverError()
         }
-      }
-      const { username } = httpRequest.user
-      const { creditedUsername, value } = httpRequest.body
-      if (username === creditedUsername) {
-        return badRequest(new InvalidParamError('creditedUsername'))
-      }
-      if (!this.currencyValidator.isValid(value)) {
-        return badRequest(new InvalidParamError('value'))
-      }
-      const transactionData = {
-        debitedUsername: username,
-        creditedUsername,
-        value
-      }
-      const record = await this.recordTransaction.record(transactionData)
-      if (!record.recorded) {
-        return badRequest(new TransactionError(record.message))
-      }
-      return okResponse(record.message)
-    } catch (error: any) {
-      return serverError()
+      default:
+        return methodNotAllowed()
     }
   }
 }
