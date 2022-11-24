f3855251d0e58bdf27066c4ccfa408f05d1d2cdf
Felipe Pedrosa <marconizero@gmail.com>
2022-11-24 13:31:41 -0300

    test: ensure BalanceController returns 405 status if not allowed method is called

--- src/presentation/controllers/balance/balance.spec.ts
+++ src/presentation/controllers/balance/balance.spec.ts
@@ -1,5 +1,5 @@
 import { BalanceModel, LoadBalance } from './balance-protocols'
-import { okResponse, serverError } from '../../helpers/http-helper'
+import { methodNotAllowed, okResponse, serverError } from '../../helpers/http-helper'
 import { BalanceController } from './balance'
 
 const makeLoadBalanceStub = (): LoadBalance => {
@@ -26,10 +26,17 @@ const makeSut = (): Subtypes => {
 }
 
 describe('Balance Controller', () => {
+  test('Should return 405 status if not allowed method is called', async () => {
+    const { sut } = makeSut()
+    const httpRequest = { user: { id: '1', username: 'any_username' }, method: 'POST' }
+    const balance = await sut.handle(httpRequest)
+    expect(balance).toEqual(methodNotAllowed())
+  })
+
   test('Should call LoadBalance with correct userId', async () => {
     const { sut, loadBalanceStub } = makeSut()
     const loadSpy = jest.spyOn(loadBalanceStub, 'load')
-    const httpRequest = { user: { id: '1', username: 'any_username' } }
+    const httpRequest = { user: { id: '1', username: 'any_username' }, method: 'GET' }
     await sut.handle(httpRequest)
     expect(loadSpy).toHaveBeenCalledWith(1)
   })
@@ -37,14 +44,14 @@ describe('Balance Controller', () => {
   test('Should return 500 status if LoadBalance throws', async () => {
     const { sut, loadBalanceStub } = makeSut()
     jest.spyOn(loadBalanceStub, 'load').mockReturnValueOnce(Promise.reject(new Error()))
-    const httpRequest = { user: { id: '1', username: 'any_username' } }
+    const httpRequest = { user: { id: '1', username: 'any_username' }, method: 'GET' }
     const balance = await sut.handle(httpRequest)
     expect(balance).toEqual(serverError())
   })
 
   test('Should return a balance on success', async () => {
     const { sut } = makeSut()
-    const httpRequest = { user: { id: '1', username: 'any_username' } }
+    const httpRequest = { user: { id: '1', username: 'any_username' }, method: 'GET' }
     const balance = await sut.handle(httpRequest)
     expect(balance).toEqual(okResponse({ balance: 'any_balance' }))
   })
