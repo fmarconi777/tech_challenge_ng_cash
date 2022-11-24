8ca1b2ab43081e4c827c58895ecb5db1448ccc37
Felipe <marconizero@gmail.com>
2022-11-23 22:11:49 -0300

    test: ensure TransactionRepository calls recordTransactionORMStub with correct values

---
+++ src/infra/db/transaction/transaction-repository.spec.ts
@@ -0,0 +1,42 @@
+import { RecordData, RecordTransactionORM } from '../../protocols/transaction/record-transaction-orm'
+import { TransactionRepository } from './transaction-repository'
+
+const recordData = {
+  debitedAccountId: 1,
+  debitedBalance: 0.00,
+  creditedAccountId: 2,
+  creditedBalance: 200.00,
+  value: 100.00
+}
+
+const makeRecordTransactionORMStub = (): RecordTransactionORM => {
+  class RecordTransactionORMStub implements RecordTransactionORM {
+    async record (recordData: RecordData): Promise<string> {
+      return await Promise.resolve('Transaction succesfully recorded')
+    }
+  }
+  return new RecordTransactionORMStub()
+}
+
+type SubTypes = {
+  sut: TransactionRepository
+  recordTransactionORMStub: RecordTransactionORM
+}
+
+const makeSut = (): SubTypes => {
+  const recordTransactionORMStub = makeRecordTransactionORMStub()
+  const sut = new TransactionRepository(recordTransactionORMStub)
+  return {
+    sut,
+    recordTransactionORMStub
+  }
+}
+
+describe('Transaction Repository', () => {
+  test('Should call recordTransactionORMStub with correct values', async () => {
+    const { sut, recordTransactionORMStub } = makeSut()
+    const recordSpy = jest.spyOn(recordTransactionORMStub, 'record')
+    await sut.record(recordData)
+    expect(recordSpy).toHaveBeenCalledWith(recordData)
+  })
+})
