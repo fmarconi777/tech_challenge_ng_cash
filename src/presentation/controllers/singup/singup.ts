61a19b8c703d3da7bf3c84140a200a2bfc2fa586
Felipe Pedrosa <marconizero@gmail.com>
2022-11-24 13:42:31 -0300

    feat: ensure SignUpController returns 405 status if not allowed method is called

--- src/presentation/controllers/singup/singup.ts
+++ src/presentation/controllers/singup/singup.ts
@@ -1,6 +1,6 @@
 import { Controller, HttpRequest, HttpResponse, Validator, AddUserAccount } from './singup-protocols'
 import { InvalidParamError, MissingParamError, UsernameInUseError } from '../../errors'
-import { badRequest, forbidden, okResponse, serverError } from '../../helpers/http-helper'
+import { badRequest, forbidden, methodNotAllowed, okResponse, serverError } from '../../helpers/http-helper'
 
 export class SignUpController implements Controller {
   constructor (
@@ -10,30 +10,36 @@ export class SignUpController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    try {
-      const requiredFields = ['username', 'password']
-      for (const field of requiredFields) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field))
+    const method = httpRequest.method
+    switch (method) {
+      case 'POST':
+        try {
+          const requiredFields = ['username', 'password']
+          for (const field of requiredFields) {
+            if (!httpRequest.body[field]) {
+              return badRequest(new MissingParamError(field))
+            }
+          }
+          const { username, password } = httpRequest.body
+          if (!this.userValidator.isValid((username as string).trim())) {
+            return badRequest(new InvalidParamError('username'))
+          }
+          if (!this.passwordValidator.isValid((password as string).trim())) {
+            return badRequest(new InvalidParamError('password'))
+          }
+          const userAccount = await this.addUserAccount.addUserAccount({
+            username,
+            password
+          })
+          if (!userAccount) {
+            return forbidden(new UsernameInUseError())
+          }
+          return okResponse(userAccount)
+        } catch (error: any) {
+          return serverError()
         }
-      }
-      const { username, password } = httpRequest.body
-      if (!this.userValidator.isValid((username as string).trim())) {
-        return badRequest(new InvalidParamError('username'))
-      }
-      if (!this.passwordValidator.isValid((password as string).trim())) {
-        return badRequest(new InvalidParamError('password'))
-      }
-      const userAccount = await this.addUserAccount.addUserAccount({
-        username,
-        password
-      })
-      if (!userAccount) {
-        return forbidden(new UsernameInUseError())
-      }
-      return okResponse(userAccount)
-    } catch (error: any) {
-      return serverError()
+      default:
+        return methodNotAllowed()
     }
   }
 }
