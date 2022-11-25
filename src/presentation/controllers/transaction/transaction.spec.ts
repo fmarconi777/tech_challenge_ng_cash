f15828d6df4f3976b2fea5038c1c637a4d0e976d
Felipe <marconizero@gmail.com>
2022-11-24 23:51:57 -0300

    test: ensure TransactionController calls LoadTransactions with correct userId

--- src/presentation/controllers/transaction/transaction.spec.ts
+++ src/presentation/controllers/transaction/transaction.spec.ts
@@ -1,3 +1,4 @@
+import { LoadTransactions, RecordsData } from '../../../domain/use-cases/transaction/load-transactions'
 import { Record, RecordTransaction, TransactionData } from '../../../domain/use-cases/transaction/record-transaction'
 import { InvalidParamError, MissingParamError } from '../../errors'
 import { TransactionError } from '../../errors/transaction-error'
@@ -38,20 +39,38 @@ const makesCurrencyValidatorStub = (): Validator => {
   return new CurrencyValidatorStub()
 }
 
+const makeLoadTransactionsStub = (): LoadTransactions => {
+  class LoadTransactionsStub implements LoadTransactions {
+    async load (id: number): Promise<RecordsData[]> {
+      return [{
+        id: 'any_id',
+        debitedUsername: 'any_debitedUsername',
+        creditedUsername: 'any_creditedUsername',
+        value: 'any_value',
+        createdAt: 'any_createdAt'
+      }]
+    }
+  }
+  return new LoadTransactionsStub()
+}
+
 type Subtypes = {
   sut: TransactionController
   recordTransactionStub: RecordTransaction
   currencyValidatorStub: Validator
+  loadTransactionsStub: LoadTransactions
 }
 
 const makeSut = (): Subtypes => {
+  const loadTransactionsStub = makeLoadTransactionsStub()
   const currencyValidatorStub = makesCurrencyValidatorStub()
   const recordTransactionStub = makeRecordTransactionStub()
-  const sut = new TransactionController(currencyValidatorStub, recordTransactionStub)
+  const sut = new TransactionController(currencyValidatorStub, recordTransactionStub, loadTransactionsStub)
   return {
     sut,
     recordTransactionStub,
-    currencyValidatorStub
+    currencyValidatorStub,
+    loadTransactionsStub
   }
 }
 
@@ -73,107 +92,119 @@ describe('Transaction Controller', () => {
     expect(httpResponse).toEqual(methodNotAllowed())
   })
 
-  test('Should return 400 status if creditedUsername is not provided', async () => {
-    const { sut } = makeSut()
-    const httpRequest = {
-      body: {
+  describe('POST method', () => {
+    test('Should return 400 status if creditedUsername is not provided', async () => {
+      const { sut } = makeSut()
+      const httpRequest = {
+        body: {
+          value: '100.00'
+        },
+        user: {
+          id: '1',
+          username: 'any_username'
+        },
+        method: 'POST'
+      }
+      const httpResponse = await sut.handle(httpRequest)
+      expect(httpResponse).toEqual(badRequest(new MissingParamError('creditedUsername')))
+    })
+
+    test('Should return 400 status if value is not provided', async () => {
+      const { sut } = makeSut()
+      const httpRequest = {
+        body: {
+          creditedUsername: 'some_username'
+        },
+        user: {
+          id: '1',
+          username: 'any_username'
+        },
+        method: 'POST'
+      }
+      const httpResponse = await sut.handle(httpRequest)
+      expect(httpResponse).toEqual(badRequest(new MissingParamError('value')))
+    })
+
+    test('Should return 400 status if username and creditedUsername are equal', async () => {
+      const { sut } = makeSut()
+      const httpRequest = {
+        body: {
+          creditedUsername: 'any_username',
+          value: '100.00'
+        },
+        user: {
+          id: '1',
+          username: 'any_username'
+        },
+        method: 'POST'
+      }
+      const httpResponse = await sut.handle(httpRequest)
+      expect(httpResponse).toEqual(badRequest(new InvalidParamError('creditedUsername')))
+    })
+
+    test('Should call CurrencyValidator with correct value', async () => {
+      const { sut, currencyValidatorStub } = makeSut()
+      const isValidSpy = jest.spyOn(currencyValidatorStub, 'isValid')
+      await sut.handle(request)
+      expect(isValidSpy).toHaveBeenCalledWith('100.00')
+    })
+
+    test('Should return 500 status if CurrencyValidator throws', async () => {
+      const { sut, currencyValidatorStub } = makeSut()
+      jest.spyOn(currencyValidatorStub, 'isValid').mockImplementationOnce(() => { throw new Error() })
+      const httpResponse = await sut.handle(request)
+      expect(httpResponse).toEqual(serverError())
+    })
+
+    test('Should 400 status if CurrencyValidator returns false', async () => {
+      const { sut, currencyValidatorStub } = makeSut()
+      jest.spyOn(currencyValidatorStub, 'isValid').mockReturnValueOnce(false)
+      const httpResponse = await sut.handle(request)
+      expect(httpResponse).toEqual(badRequest(new InvalidParamError('value')))
+    })
+
+    test('Should call RecordTransaction with correct values', async () => {
+      const { sut, recordTransactionStub } = makeSut()
+      const recordSpy = jest.spyOn(recordTransactionStub, 'record')
+      await sut.handle(request)
+      expect(recordSpy).toHaveBeenCalledWith({
+        debitedUsername: 'any_username',
+        creditedUsername: 'some_username',
         value: '100.00'
-      },
-      user: {
-        id: '1',
-        username: 'any_username'
-      },
-      method: 'POST'
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new MissingParamError('creditedUsername')))
-  })
+      })
+    })
 
-  test('Should return 400 status if value is not provided', async () => {
-    const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        creditedUsername: 'some_username'
-      },
-      user: {
-        id: '1',
-        username: 'any_username'
-      },
-      method: 'POST'
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new MissingParamError('value')))
-  })
+    test('Should return 500 status if RecordTransaction throws', async () => {
+      const { sut, recordTransactionStub } = makeSut()
+      jest.spyOn(recordTransactionStub, 'record').mockReturnValueOnce(Promise.reject(new Error()))
+      const httpResponse = await sut.handle(request)
+      expect(httpResponse).toEqual(serverError())
+    })
 
-  test('Should return 400 status if username and creditedUsername are equal', async () => {
-    const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        creditedUsername: 'any_username',
-        value: '100.00'
-      },
-      user: {
-        id: '1',
-        username: 'any_username'
-      },
-      method: 'POST'
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new InvalidParamError('creditedUsername')))
-  })
+    test('Should return 400 status if RecordTransaction recorded returns false', async () => {
+      const { sut, recordTransactionStub } = makeSut()
+      jest.spyOn(recordTransactionStub, 'record').mockReturnValueOnce(Promise.resolve({
+        recorded: false,
+        message: 'any_message'
+      }))
+      const httpResponse = await sut.handle(request)
+      expect(httpResponse).toEqual(badRequest(new TransactionError('any_message')))
+    })
 
-  test('Should call CurrencyValidator with correct value', async () => {
-    const { sut, currencyValidatorStub } = makeSut()
-    const isValidSpy = jest.spyOn(currencyValidatorStub, 'isValid')
-    await sut.handle(request)
-    expect(isValidSpy).toHaveBeenCalledWith('100.00')
-  })
-
-  test('Should return 500 status if CurrencyValidator throws', async () => {
-    const { sut, currencyValidatorStub } = makeSut()
-    jest.spyOn(currencyValidatorStub, 'isValid').mockImplementationOnce(() => { throw new Error() })
-    const httpResponse = await sut.handle(request)
-    expect(httpResponse).toEqual(serverError())
-  })
-
-  test('Should 400 status if CurrencyValidator returns false', async () => {
-    const { sut, currencyValidatorStub } = makeSut()
-    jest.spyOn(currencyValidatorStub, 'isValid').mockReturnValueOnce(false)
-    const httpResponse = await sut.handle(request)
-    expect(httpResponse).toEqual(badRequest(new InvalidParamError('value')))
-  })
-
-  test('Should call RecordTransaction with correct values', async () => {
-    const { sut, recordTransactionStub } = makeSut()
-    const recordSpy = jest.spyOn(recordTransactionStub, 'record')
-    await sut.handle(request)
-    expect(recordSpy).toHaveBeenCalledWith({
-      debitedUsername: 'any_username',
-      creditedUsername: 'some_username',
-      value: '100.00'
+    test('Should return a message on success', async () => {
+      const { sut } = makeSut()
+      const httpResponse = await sut.handle(request)
+      expect(httpResponse).toEqual(okResponse('Transaction succesfully recorded'))
     })
   })
 
-  test('Should return 500 status if RecordTransaction throws', async () => {
-    const { sut, recordTransactionStub } = makeSut()
-    jest.spyOn(recordTransactionStub, 'record').mockReturnValueOnce(Promise.reject(new Error()))
-    const httpResponse = await sut.handle(request)
-    expect(httpResponse).toEqual(serverError())
-  })
-
-  test('Should return 400 status if RecordTransaction recorded returns false', async () => {
-    const { sut, recordTransactionStub } = makeSut()
-    jest.spyOn(recordTransactionStub, 'record').mockReturnValueOnce(Promise.resolve({
-      recorded: false,
-      message: 'any_message'
-    }))
-    const httpResponse = await sut.handle(request)
-    expect(httpResponse).toEqual(badRequest(new TransactionError('any_message')))
-  })
-
-  test('Should return a message on success', async () => {
-    const { sut } = makeSut()
-    const httpResponse = await sut.handle(request)
-    expect(httpResponse).toEqual(okResponse('Transaction succesfully recorded'))
+  describe('GET method', () => {
+    test('Should call LoadTransactions with correct userId', async () => {
+      const { sut, loadTransactionsStub } = makeSut()
+      const loadSpy = jest.spyOn(loadTransactionsStub, 'load')
+      const httpRequest = { user: { id: '1', username: 'any_username' }, method: 'GET' }
+      await sut.handle(httpRequest)
+      expect(loadSpy).toHaveBeenCalledWith(1)
+    })
   })
 })
