ae1d1edd79ed869f9f12a57abf2b32ebc11d718c
Felipe Pedrosa <marconizero@gmail.com>
2022-11-25 11:41:39 -0300

    test: ensure TransactionRepository calls LoadTransactionsByIdORM with correct values

--- src/infra/db/transaction/transaction-repository.spec.ts
+++ src/infra/db/transaction/transaction-repository.spec.ts
@@ -1,3 +1,4 @@
+import { LoadTransactionsByIdORM, RecordsData } from '../../protocols/transaction/load-transactions-orm'
 import { RecordData, RecordTransactionORM } from '../../protocols/transaction/record-transaction-orm'
 import { TransactionRepository } from './transaction-repository'
 
@@ -18,38 +19,61 @@ const makeRecordTransactionORMStub = (): RecordTransactionORM => {
   return new RecordTransactionORMStub()
 }
 
+const makeLoadTransactionsByIdORMStub = (): LoadTransactionsByIdORM => {
+  class LoadTransactionsByIdORMStub implements LoadTransactionsByIdORM {
+    async loadById (id: number): Promise<RecordsData[]> {
+      return await Promise.resolve([])
+    }
+  }
+  return new LoadTransactionsByIdORMStub()
+}
+
 type SubTypes = {
   sut: TransactionRepository
   recordTransactionORMStub: RecordTransactionORM
+  loadTransactionsByIdORMStub: LoadTransactionsByIdORM
 }
 
 const makeSut = (): SubTypes => {
+  const loadTransactionsByIdORMStub = makeLoadTransactionsByIdORMStub()
   const recordTransactionORMStub = makeRecordTransactionORMStub()
-  const sut = new TransactionRepository(recordTransactionORMStub)
+  const sut = new TransactionRepository(recordTransactionORMStub, loadTransactionsByIdORMStub)
   return {
     sut,
-    recordTransactionORMStub
+    recordTransactionORMStub,
+    loadTransactionsByIdORMStub
   }
 }
 
 describe('Transaction Repository', () => {
-  test('Should call recordTransactionORMStub with correct values', async () => {
-    const { sut, recordTransactionORMStub } = makeSut()
-    const recordSpy = jest.spyOn(recordTransactionORMStub, 'record')
-    await sut.record(recordData)
-    expect(recordSpy).toHaveBeenCalledWith(recordData)
+  describe('Record method', () => {
+    test('Should call recordTransactionORMStub with correct values', async () => {
+      const { sut, recordTransactionORMStub } = makeSut()
+      const recordSpy = jest.spyOn(recordTransactionORMStub, 'record')
+      await sut.record(recordData)
+      expect(recordSpy).toHaveBeenCalledWith(recordData)
+    })
+
+    test('Should throw if recordTransactionORMStub throws', async () => {
+      const { sut, recordTransactionORMStub } = makeSut()
+      jest.spyOn(recordTransactionORMStub, 'record').mockReturnValueOnce(Promise.reject(new Error()))
+      const record = sut.record(recordData)
+      await expect(record).rejects.toThrow()
+    })
+
+    test('Should return a message on success', async () => {
+      const { sut } = makeSut()
+      const userAccount = await sut.record(recordData)
+      expect(userAccount).toEqual('Transaction succesfully recorded')
+    })
   })
 
-  test('Should throw if recordTransactionORMStub throws', async () => {
-    const { sut, recordTransactionORMStub } = makeSut()
-    jest.spyOn(recordTransactionORMStub, 'record').mockReturnValueOnce(Promise.reject(new Error()))
-    const record = sut.record(recordData)
-    await expect(record).rejects.toThrow()
-  })
-
-  test('Should return a message on success', async () => {
-    const { sut } = makeSut()
-    const userAccount = await sut.record(recordData)
-    expect(userAccount).toEqual('Transaction succesfully recorded')
+  describe('LoadById method', () => {
+    test('Should call LoadTransactionsByIdORM with correct values', async () => {
+      const { sut, loadTransactionsByIdORMStub } = makeSut()
+      const loadByIdSpy = jest.spyOn(loadTransactionsByIdORMStub, 'loadById')
+      await sut.loadById(1)
+      expect(loadByIdSpy).toHaveBeenCalledWith(1)
+    })
   })
 })
