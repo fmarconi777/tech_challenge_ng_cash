60936b95ce450999329188770909dc25712a4934
Felipe <marconizero@gmail.com>
2022-11-27 00:00:05 -0300

    feat: ensure that TransactionFilterController returns 400 status if not allowed param is provided

--- src/presentation/controllers/transaction/transaction-filter.ts
+++ src/presentation/controllers/transaction/transaction-filter.ts
@@ -1,12 +1,18 @@
-import { methodNotAllowed } from '../../helpers/http-helper'
+import { InvalidParamError } from '../../errors'
+import { badRequest, methodNotAllowed } from '../../helpers/http-helper'
 import { HttpRequest, HttpResponse } from '../../protocols'
 import { Controller } from './transaction-protocols'
 
 export class TransactionFilterController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const method = httpRequest.method
+    const param = httpRequest.param
     switch (method) {
       case 'GET':
+        switch (param) {
+          default:
+            return badRequest(new InvalidParamError('expected date, cashIn or cashOut params'))
+        }
       default:
         return methodNotAllowed()
     }
