47b1cdd74a0a9b52bfe94bf93604148ede6f753f
Felipe <marconizero@gmail.com>
2022-11-27 01:05:32 -0300

    test: ensure that DbLoadFilteredCashTransactions calls LoadUserByIdRepository with correct value

---
+++ src/data/use-cases/transaction/db-load-filtered-cash-transactions/db-load-filtered-cash-transactions.spec.ts
@@ -0,0 +1,47 @@
+import { UserModel } from '../../../../domain/models/user'
+import { LoadUserByIdRepository } from '../../../protocols/db/user/load-user-by-id-repository'
+import { DbLoadFilteredCashTransactions } from './db-load-filtered-cash-transactions'
+
+const filterData = {
+  userId: 1,
+  filter: 'any_param'
+}
+
+const fakeUser = {
+  id: '1',
+  username: 'any_username',
+  password: 'any_password',
+  accountId: '1'
+}
+
+const makeLoadUserByIdRepositoryStub = (): LoadUserByIdRepository => {
+  class LoadUserByIdRepositoryStub implements LoadUserByIdRepository {
+    async loadById (id: number): Promise<UserModel | null> {
+      return await Promise.resolve(fakeUser)
+    }
+  }
+  return new LoadUserByIdRepositoryStub()
+}
+
+type SubTypes = {
+  sut: DbLoadFilteredCashTransactions
+  loadUserByIdRepositoryStub: LoadUserByIdRepository
+}
+
+const makeSut = (): SubTypes => {
+  const loadUserByIdRepositoryStub = makeLoadUserByIdRepositoryStub()
+  const sut = new DbLoadFilteredCashTransactions(loadUserByIdRepositoryStub)
+  return {
+    sut,
+    loadUserByIdRepositoryStub
+  }
+}
+
+describe('DbLoadFilteredCashTransactions', () => {
+  test('Should call LoadUserByIdRepository with correct value', async () => {
+    const { sut, loadUserByIdRepositoryStub } = makeSut()
+    const loadByIdSpy = jest.spyOn(loadUserByIdRepositoryStub, 'loadById')
+    await sut.load(filterData)
+    expect(loadByIdSpy).toHaveBeenCalledWith(1)
+  })
+})
