4a51aa47a43715f602414d163ef2b7836b1cf6f4
Felipe Pedrosa <marconizero@gmail.com>
2022-11-29 15:38:32 -0300

    feat: ensure TransactionRepository calls LoadFilterByDateTransactionsORM with correct values

--- src/infra/db/transaction/transaction-repository.ts
+++ src/infra/db/transaction/transaction-repository.ts
@@ -1,16 +1,20 @@
+import { LoadFilterByDateTransactionsRepository, PeriodData } from '../../../data/protocols/db/transaction/load-filter-by-date-transactions-repository'
 import { FilterValues, LoadFilteredCashTransactionsRepository } from '../../../data/protocols/db/transaction/load-filtered-cash-transactions-repository'
 import { LoadTransactionsByAccountIdRepository, RecordsData } from '../../../data/protocols/db/transaction/load-transactions-by-account-id-repository'
 import { RecordData, RecordTransactionRepository } from '../../../data/protocols/db/transaction/record-transaction-repository'
+import { LoadFilterByDateTransactionsORM } from '../../protocols/transaction/load-filter-by-date-transactions-orm'
 import { LoadTransactionsByAccountIdORM, RecordTransactionORM, LoadFilteredCashTransactionsORM } from './transaction-repository-protocols'
 
 export class TransactionRepository implements
   RecordTransactionRepository,
   LoadTransactionsByAccountIdRepository,
-  LoadFilteredCashTransactionsRepository {
+  LoadFilteredCashTransactionsRepository,
+  LoadFilterByDateTransactionsRepository {
   constructor (
     private readonly recordTransactionORM: RecordTransactionORM,
     private readonly loadTransactionsByAccountIdORM: LoadTransactionsByAccountIdORM,
-    private readonly loadFilteredCashTransactionsORMStub: LoadFilteredCashTransactionsORM
+    private readonly loadFilteredCashTransactionsORM: LoadFilteredCashTransactionsORM,
+    private readonly loadFilterByDateTransactionsORM: LoadFilterByDateTransactionsORM
   ) {}
 
   async record (recordData: RecordData): Promise<string> {
@@ -22,6 +26,11 @@ export class TransactionRepository implements
   }
 
   async loadByFilter (filterValues: FilterValues): Promise<RecordsData[]> {
-    return await this.loadFilteredCashTransactionsORMStub.loadByFilter(filterValues)
+    return await this.loadFilteredCashTransactionsORM.loadByFilter(filterValues)
+  }
+
+  async loadByFilterDate (periodData: PeriodData): Promise<RecordsData[]> {
+    await this.loadFilterByDateTransactionsORM.loadByFilterDate(periodData)
+    return []
   }
 }
---
+++ src/infra/protocols/transaction/load-filter-by-date-transactions-orm.ts
@@ -0,0 +1,6 @@
+import { PeriodData } from '../../../data/protocols/db/transaction/load-filter-by-date-transactions-repository'
+import { RecordsData } from '../../../domain/use-cases/transaction/load-transactions/load-transactions'
+
+export interface LoadFilterByDateTransactionsORM {
+  loadByFilterDate: (periodData: PeriodData) => Promise<RecordsData[]>
+}
