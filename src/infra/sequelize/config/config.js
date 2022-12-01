ba02ed6b206fd620e121513f806aeb596e65b32e
Felipe Pedrosa <marconizero@gmail.com>
2022-12-01 11:55:55 -0300

    chore: change dev scriptto use docker-compose

--- src/infra/sequelize/config/config.js
+++ src/infra/sequelize/config/config.js
@@ -17,13 +17,5 @@ module.exports = {
     host: process.env.DB_TEST_HOST,
     dialect: process.env.DB_TEST_DIALECT,
     logging: false
-  },
-  production: {
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    logging: false
   }
 }
